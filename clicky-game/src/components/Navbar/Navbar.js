3b12bd86f852a9bad8e456db163bdbbc1fb9b1d3
murnda <mdelong333@gmail.com>
2020-01-11 23:35:02 -0500

    starting on navbar

--- clicky-game/src/components/Navbar/Navbar.js
+++ clicky-game/src/components/Navbar/Navbar.js
@@ -4,11 +4,11 @@ import "./style.css";
 function Navbar () {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul>
-                <li className="logo">Clicky Game</li>
-                <li></li>
-                <li></li>
-            </ul>
+            <div className="navbar-nav">
+                <div className="logo navbar-brand">Clicky Game</div>
+                <div className="message nav-item"></div>
+                <div className="score nav-item"></div>
+            </div>
         </nav>
     );
 };
