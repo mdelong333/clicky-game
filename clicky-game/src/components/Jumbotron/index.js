65564befe6e5059b8bfaf81ba119eac7523221c3
murnda <mdelong333@gmail.com>
2020-01-13 23:40:00 -0500

    added font/caught up in styling

--- clicky-game/src/components/Jumbotron/index.js
+++ clicky-game/src/components/Jumbotron/index.js
@@ -5,7 +5,9 @@ function Jumbotron(props) {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
-                <h1 className="display-4">Clicky Game!</h1>
+                <h1 className="display-4">Sailor Moon Clicky Game!</h1>
+                <br/>
+                <br/>
                 <p className="lead">Click on an image to earn points, but don't click any image more than once!</p>
             </div>
         </div>
--- clicky-game/src/components/Navbar/index.js
+++ clicky-game/src/components/Navbar/index.js
@@ -4,11 +4,11 @@ import "./style.css";
 function Navbar (props) {
     return (
         <nav className="navbar navbar-expand-sm">
-            <ul className="navbar-nav flex-container">
+            <div className="navbar-nav flex-container">
                 <h5 className="nav-item logo">Clicky Game</h5>
-                <li className="message nav-item"></li>
-                <li className="score nav-item">Score: {props.score} || High Score: {props.highScore}</li>
-            </ul>
+                <h5 className="message nav-item"></h5>
+                <h5 className="score nav-item">Score: {props.score} | High Score: {props.highScore}</h5>
+            </div>
         </nav>
     );
 };
