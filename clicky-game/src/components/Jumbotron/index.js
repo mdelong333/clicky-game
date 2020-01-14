af995eed63d000ccff7c0f33a6a3f186c1c226cd
murnda <mdelong333@gmail.com>
2020-01-13 23:57:09 -0500

    more styling

--- clicky-game/src/components/Jumbotron/index.js
+++ clicky-game/src/components/Jumbotron/index.js
@@ -3,12 +3,12 @@ import "./style.css";
 
 function Jumbotron(props) {
     return (
-        <div className="jumbotron jumbotron-fluid">
+        <div className="jumbotron jumbotron-fluid double-border">
             <div className="container">
                 <h1 className="display-4">Sailor Moon Clicky Game!</h1>
                 <br/>
                 <br/>
-                <p className="lead">Click on an image to earn points, but don't click any image more than once!</p>
+                <p>Click on an image to earn points, but don't click any image more than once!</p>
             </div>
         </div>
     )
