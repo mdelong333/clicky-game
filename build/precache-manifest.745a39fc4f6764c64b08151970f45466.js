5e38444edbdffff693105c235228ada85cba4b13
murnda <mdelong333@gmail.com>
2020-01-14 13:59:11 -0500

    still having problems

---
+++ build/precache-manifest.745a39fc4f6764c64b08151970f45466.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b8b6b2d21d0eb1036777a6e59f43b2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "454ae3bfbaf851ea5b4e",
+    "url": "/clicky-game/static/css/main.80784869.chunk.css"
+  },
+  {
+    "revision": "a4d43337e60fc5f4725c",
+    "url": "/clicky-game/static/js/2.26f80e77.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.26f80e77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "454ae3bfbaf851ea5b4e",
+    "url": "/clicky-game/static/js/main.ccf3903d.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  },
+  {
+    "revision": "ffc8b4cfe1cdf2f52b93999101479997",
+    "url": "/clicky-game/static/media/caslon.ffc8b4cf.woff"
+  },
+  {
+    "revision": "7ba63fb4753cfeab88df3df63e147512",
+    "url": "/clicky-game/static/media/sailorMoon.7ba63fb4.ttf"
+  }
+]);
