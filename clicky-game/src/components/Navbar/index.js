4b9ae48de7b84010b4750078ec0c8cc9bfefa9b7
murnda <mdelong333@gmail.com>
2020-01-14 01:17:53 -0500

    nav text responsive

--- clicky-game/src/components/Navbar/index.js
+++ clicky-game/src/components/Navbar/index.js
@@ -3,13 +3,11 @@ import "./style.css";
 
 function Navbar (props) {
     return (
-        <nav className="navbar navbar-expand-sm">
-            <div className="navbar-nav flex-container">
-                <h5 className="nav-item logo">Clicky Game</h5>
-                <h5 className="message nav-item">{props.message}</h5>
-                <h5 className="score nav-item">Score: {props.score}</h5>
-                <h5 className="score nav-item">|</h5>
-                <h5 className="score nav-item">High Score: {props.highScore}</h5>
+        <nav className="navbar">
+            <div className="row container-fluid">
+                <h3 className="nav-item logo">Clicky Game</h3>
+                <h4 className="message nav-item col-6">{props.message}</h4>
+                <h3 className="score nav-item">Score: {props.score} | High Score: {props.highScore}</h3>
             </div>
         </nav>
     );
